3b87f36fdf7e290527f297539a05ef667c0f89d2
Juli <simijulcsi@yahoo.com>
2020-08-31 22:31:39 +0200

    form picture and form added

--- form.php
+++ form.php
@@ -1,5 +1,30 @@
 <?php include 'header.php' ?>
-<p>Hi, I am the form</p>
-</body>
+
+<div class="container-fluid form-background">
+    <div class="page-heading">
+    <!-- Page Heading -->
+    <h2>Animals</h2>
+    </div>
+</div>
+<section class="container-fluid">
+    <div class="animal-form">
+        <form action="#" >
+        <div class="input-group">
+            <span class="input-group-addon" id="animal-name">Név</span>
+            <input type="text" class="form-control" placeholder="Lia" aria-describedby="basic-addon1">
+        </div>
+        <div class="input-group">
+            <span class="input-group-addon" id="animal-breed">Fajta</span>
+            <input type="text" class="form-control" placeholder="Golden Retriever" aria-describedby="basic-addon1">
+        </div>  
+        <div class="input-group">
+            <span class="input-group-addon" id="animal-age">Életkor</span>
+            <input type="number" class="form-control" placeholder="2 " aria-describedby="basic-addon1">
+        </div>
+            <input type="submit" value="DELETE" class="btn btn-default delete-button">  
+        </form>
+    </div>
+
+</section>
 
 <?php include 'footer.php' ?>
